33e9c1f801ebfbeb9fe68ef014044177c3607d08
kaneki33 <abdulaziz331998@gmail.com>
2019-03-04 22:00:40 +0300

    second

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '776839207:AAGPOS9RH1n0fFwqhp-W7xfTUGVXdUVaXRY';
 
 var bot = new TelegramBot(token, {polling: true});
+bot.sendMessage(737446966, `  onlne: \n`);
 bot.onText(/(.+)/, (msg, match) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
