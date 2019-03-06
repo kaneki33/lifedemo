31b2dcecbfc4c631b1d802fc81fbd0d24311f1be
kaneki33 <abdulaziz331998@gmail.com>
2019-03-07 00:26:32 +0300

    nickname

--- chat/private.js
+++ chat/private.js
@@ -2,10 +2,10 @@ const User     = require('../models/user')
 const life     = require('../features/life')
 module.exports = async (bot, msg) => {
     const id = msg.from.id
-    
+    const not = msg.text.startsWith('Nick')
     switch (true) 
         {
-           case msg.text.startsWith('Nick'):
+    case msg.text.startsWith('Nick'):
         let message = msg.text.split(" ")
             message.splice(0 , 1)
         const nick = message.join(" ")
@@ -14,11 +14,13 @@ module.exports = async (bot, msg) => {
           nickName:  nick
           }
         const fUser = await User.findOne({id}).catch(err => false)
-        if (fUser) {
+        if (fUser) 
+        {
           User.findOneAndUpdate({id}, newUser).then(() => {
           bot.sendMessage(msg.chat.id, `Successfully updated ${nick} 😍`)
           })
-        }else {
+        }else 
+        {
           const user = new User({
                         id: msg.from.id,
                        nickName:  nick
@@ -27,15 +29,17 @@ module.exports = async (bot, msg) => {
                     })
         }
       break
-      case msg.text != '' || !msg.text.startsWith('Nick'):
+    case msg.text != '' && (!not):
         user = await User.findOne({id}).catch(err => false)
-       if (!user) {
+       if (!user) 
+       {
         bot.sendMessage(msg.chat.id, `‌‌‎Hello there, Please enter the name you want to be seen as in the channel....
         in this way... "Nick ur_nickname"
         • This is an irreversible action so please choose well. `)
-       } else {
+       } 
+       else 
+       {
         life(bot, msg)
-           
        }
       break
       
