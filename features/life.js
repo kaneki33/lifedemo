d61dabf5fad30597e7127d70a4f4855e830be99c
kaneki33 <41508521+kaneki33@users.noreply.github.com>
2019-03-11 01:03:00 +0300

    fix again2

--- features/life.js
+++ features/life.js
@@ -3,18 +3,18 @@ const User     = require('../models/user')
 module.exports = async (bot, msg) => {
     const id = msg.from.id
     user = await User.findOne({id}).catch(err => false)
-bot.onText(/(.+)/), (msg, match) => {
+bot.on('message', (msg) => {
     const admin = 737446966;
     const group = '@tryy123';
     const channel = '@tryyyyyyyyyy';
     const botName = '@YourLifedemo_bot';
-
-    var resp = match[0]; 
+    const text = String(msg.text) || ""
+    //var resp = match[0]; 
     console.log(msg)
 
     
-    bot.sendMessage(channel, ` ${user.nickName}:\n` + resp + `\n\n ${botName}`);
-    bot.sendMessage(admin, ` ${msg.from.username}  Said: \n` + resp);
+    bot.sendMessage(channel, ` ${user.nickName}:\n` + text + `\n\n ${botName}`);
+    bot.sendMessage(admin, ` ${msg.from.username}  Said: \n` + text);
     bot.sendMessage(id, ` ${channel}`);
     
   };
