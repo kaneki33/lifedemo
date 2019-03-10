64aaed371bc950f2337dc9986ce6b5c1f33217b1
kaneki33 <41508521+kaneki33@users.noreply.github.com>
2019-03-11 00:49:38 +0300

    fix

--- features/life.js
+++ features/life.js
@@ -9,13 +9,13 @@ bot.onText(/(.+)/, (msg, match) => {
     const channel = '@tryyyyyyyyyy';
     const botName = '@YourLifedemo_bot';
 
-    const resp = match[0]; 
+    var resp = match[0]; 
     console.log(msg)
 
     
     bot.sendMessage(channel, ` ${user.nickName}:\n` + resp + `\n\n ${botName}`);
     bot.sendMessage(admin, ` ${msg.from.username}  Said: \n` + resp);
-    bot.sendMessage(id, ` ${botName}`);
+    bot.sendMessage(id, ` ${channel}`);
     
   });
 
