5ba8be0742ba87bb802d07b02d342ae5c3bc9ea1
kaneki33 <41508521+kaneki33@users.noreply.github.com>
2019-03-11 03:41:11 +0300

    update

--- bot.js
+++ bot.js
@@ -38,6 +38,7 @@ const id = msg.from.id
                       User.findOneAndUpdate({id}, newUser).then(() => {
                       bot.sendMessage(msg.chat.id, `Successfully updated ${nick} 😍`)
                       })
+                bot.sendMessage(msg.chat.id, `Successfully updated ${nick} `
             }
             else
             {
