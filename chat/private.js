e4dd68de41459c60be198d31b92b93be47ce657a
kaneki33 <41508521+kaneki33@users.noreply.github.com>
2019-03-16 16:58:16 +0300

    Update private.js

--- chat/private.js
+++ chat/private.js
@@ -16,7 +16,7 @@ switch (true){
             message.splice(0 , 1)
             const nick = message.join(" ")
            // if(nick.includes(['1','2','3','4','5','6','7','8','9','',' ','.','@','(',')','#','!','-','=','*','-']))
-            if( (/\d/.test(nick) && /[A-z]/.test(nick)) || /[A-z]/.test(nick))
+            if( (/\d/.test(nick) && /[A-z]/.test(nick) && !/\W/.test(nick)) || /[A-z]/.test(nick) )
             {
                 const user = new User({
                     id: msg.from.id,
