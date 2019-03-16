ca67247973a32321bbbc6682d6a9335aabf69e7c
kaneki33 <41508521+kaneki33@users.noreply.github.com>
2019-03-16 15:01:41 +0300

    Update private.js

--- chat/private.js
+++ chat/private.js
@@ -1,13 +1,12 @@
 const User     = require('../models/user')
 module.exports = async (bot, msg) => {
-    const id = msg.from.id
-    const channel = '@tryyyyyyyyyy';
-    const admin = 737446966;
-    switch (true) 
-        {
-            
+const id = msg.from.id
+const channel = '@tryyyyyyyyyy';
+const admin = 737446966;
+switch (true) 
+    {        
     case msg.text.startsWith('Nick'):
-                const fUser = await User.findOne({id}).catch(err => false)
+        const fUser = await User.findOne({id}).catch(err => false)
         if (fUser) 
         {
           break
@@ -16,43 +15,40 @@ module.exports = async (bot, msg) => {
         {
             let message = msg.text.split(" ")
             message.splice(0 , 1)
-        const nick = message.join(" ")
-       // if(nick.includes(['1','2','3','4','5','6','7','8','9','',' ','.','@','(',')','#','!','-','=','*','-']))
-        if((nick.match(/\d/) && nick.match(/\w/)) || nick.match(/\w/))
-        {
-            const user = new User({
-                        id: msg.from.id,
-                       nickName:  nick
-                      }).save(() => {
-                      bot.sendMessage(msg.chat.id, `Successfully saved ... welcome ${nick} 😍`)
-                      bot.sendMessage(msg.chat.id, `Say Anything .. Have fun!\n${channel}`)
-                    });
+            const nick = message.join(" ")
+           // if(nick.includes(['1','2','3','4','5','6','7','8','9','',' ','.','@','(',')','#','!','-','=','*','-']))
+            if((nick.match(/\d/) && nick.match(/\w/)) || nick.match(/\w/))
+            {
+                const user = new User({
+                    id: msg.from.id,
+                    nickName:  nick
+                    }).save(() => {
+                              bot.sendMessage(msg.chat.id, `Successfully saved ... welcome ${nick} 😍`)
+                              bot.sendMessage(msg.chat.id, `Say Anything .. Have fun!\n${channel}`)
+                        });
                     bot.sendMessage(admin, `( ${nick} ) joined .. @${msg.from.username} ,${msg.from.first_name} ${msg.from.last_name} `);
                     bot.sendMessage(channel, `( ${nick} ) joined us .. Welcome ${nick} `);
-                }
-        }
+            }
             else
             {
-           bot.sendMessage(msg.chat.id, `Your nick name can not only be numbers!\nTry again!`)
-          
-        }
-      break
+                bot.sendMessage(msg.chat.id, `Your nick name can not only be numbers!\nTry again!`)
+            }
+        break
     case msg.text != '' :
         user = await User.findOne({id}).catch(err => false)
-       if (!user )
-            {
-                bot.sendMessage(msg.chat.id, `‌‌‎Hello there🤗🤗, please enter the name you want to be seen as in the #channel....
-👉This way "Nick yournickname".👈
+        if (!user ){
+            bot.sendMessage(msg.chat.id, `‌‌‎Hello there🤗🤗, please enter the name you want to be seen as in the #channel....
+            👉This way "Nick yournickname".👈
 
-This is an irreversible action‼️
+            This is an irreversible action‼️
 
-So please choose well. 
+            So please choose well. 
 
-Thank you😊😊`)
-            } 
+            Thank you😊😊`)
+        } 
             
     
-        default:
+    default:
         break
         }
     }
