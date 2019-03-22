77580601f774961f4aff4b9963da839292c42f5f
kaneki33 <abdulaziz331998@gmail.com>
2019-03-22 15:18:08 +0300

    Update admin.js

--- chat/admin.js
+++ chat/admin.js
@@ -15,10 +15,15 @@ break
           const matches = text.match(/\/Del(\s+)(.+)/)
           const nickName = matches[2]
           User.findOneAndRemove({nickName}).then((success) => {
-            if (success) {
-                bot.sendMessage(msg.chat.id, 'Nick-name deleted')
-            }else {
-                bot.sendMessage(msg.chat.id, ' Nick-name not found')
+            if (success) 
+            {
+                bot.sendMessage(msg.chat.id, 'Nick-name deleted1')
+                bot.sendMessage(msg.chat.id, 'Nick-name deleted2');
+                bot.sendMessage(msg.chat.id, 'Nick-name deletedddd');
+            }
+            else 
+            {
+                bot.sendMessage(msg.chat.id, ' Nick-name not found');
             }
           })
         } catch (e) {
