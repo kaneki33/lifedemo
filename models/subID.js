7a9f53e5b3788b098ca56895090e1469db07610e
kaneki33 <abdulaziz331998@gmail.com>
2019-03-22 18:33:45 +0300

    Update subID.js

--- models/subID.js
+++ models/subID.js
@@ -1,20 +1,18 @@
-const mongoose = require('mongoose')
-autoIncrement = require('mongoose-auto-increment');
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-auto-increment');
 mongoose.connect('mongodb://kaneki:ken1998@ds217002.mlab.com:17002/nickname?authSource=nickname&w=1&w=1', { useNewUrlParser: true })
 mongoose.Promise = global.Promise
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 let Schema = mongoose.Schema
-//autoIncrement.initialize(db);
 
 let SubIDSchema = new Schema({
      id: {type:String , required:true},
      Firstname: {type:String , required:true}
 
 });
-//SubIDSchema.plugin(autoIncrement.plugin, 'Subid');
-
+SubIDSchema.plugin(AutoIncrement, {inc_field: 'id'});
 
 
 module.exports = mongoose.model('SubID', SubIDSchema)
