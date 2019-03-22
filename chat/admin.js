e8fe93b0cfff3f2ada4d81a7eaf4dee62ca84466
kaneki33 <abdulaziz331998@gmail.com>
2019-03-22 14:33:46 +0300

    Create admin.js

---
+++ chat/admin.js
@@ -0,0 +1,34 @@
+const User = require('../models/user')
+switch (true) {
+    case text.startsWith('\/Send'):
+
+        const matches = text.match(/\/Send(\s+)(.+)(\s+)-(\s+)(.+)/)
+        var idd = matches[2];
+        var txt = matches[5];
+        bot.sendMessage(idd, txt);
+
+    case text.startsWith('\/Del'):
+
+        try {
+          const matches = text.match(/\/Del(\s+)(.+)/)
+          const nickName = matches[2]
+          User.findOneAndRemove({nickName}).then((success) => {
+            if (success) {
+                bot.sendMessage(msg.chat.id, 'Nick-name deleted')
+            }else {
+                bot.sendMessage(msg.chat.id, ' Nick-name not found')
+            }
+          })
+        } catch (e) {
+          const error = `
+          Please type in the correct format
+                Example:
+                Del "Nick-name to be deleted"
+              `
+          bot.sendMessage(msg.chat.id, error, {
+            reply_to_message_id: msg.message_id
+          })
+        }
+        break
+
+};
