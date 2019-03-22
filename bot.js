cbbbbfc8d3dd0642b3133ce36b2103b35d375a94
kaneki33 <abdulaziz331998@gmail.com>
2019-03-22 13:22:43 +0300

    tryy

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const User     = require('./models/user')
 const private = require('./chat/private')
 const life     = require('./features/life')
 const token   = '776839207:AAGPOS9RH1n0fFwqhp-W7xfTUGVXdUVaXRY'
+const id = msg.from.id
 // process.env.TOKEN
 let bot
 if (process.env.NODE_ENV === 'production') {
@@ -18,14 +19,45 @@ bot.sendMessage(737446966,"The bot is online")
 
 
 bot.on('message', (msg) => {
-   switch (true) {
-     case (msg.chat.type == 'private'):
+  
        private(bot, msg)
-       life(bot, msg)
-       break
-     default:
-       break
-   }
- });
- 
+     });
+
+     bot.on('message', async (msg) => {
+
+            user = await User.findOne({id}).catch(err => false)
+            if(msg.text.startsWith('Nic'))
+              {
+                          let message = msg.text.split(" ")
+                          message.splice(0 , 1)
+                      const nick = message.join(" ")
+                      const newUser = {
+                        id: msg.from.id,
+                        nickName:  nick
+                        }
+
+                      User.findOneAndUpdate({id}, newUser).then(() => {
+                      bot.sendMessage(msg.chat.id, `Successfully updated ${nick} 😍`)
+                      })
+            }
+            else
+            {
+                    
+                      
+                    const admin = 737446966;
+                    const group = '@tryy123';
+                    const channel = '@tryyyyyyyyyy';
+                    const botName = '@YourLifedemo_bot';
+                    const text = String(msg.text) || ""
+                    //var resp = match[0]; 
+                    console.log(msg)
+            
+                    
+                    bot.sendMessage(channel, ` ${user.nickName}:\n` + text + `\n\n ${botName}`);
+                    bot.sendMessage(admin, ` ${msg.from.username}  Said: \n` + text);
+                    bot.sendMessage(id, ` ${channel}`);
+                    
+            }
+          }); 
+        
  module.exports = bot
--- chat/private.js
+++ chat/private.js
@@ -1,23 +1,17 @@
 const User     = require('../models/user')
-const life     = require('../features/life')
 module.exports = async (bot, msg) => {
     const id = msg.from.id
     switch (true) 
         {
+            
     case msg.text.startsWith('Nick'):
         let message = msg.text.split(" ")
             message.splice(0 , 1)
         const nick = message.join(" ")
-        const newUser = {
-          id: msg.from.id,
-          nickName:  nick
-          }
         const fUser = await User.findOne({id}).catch(err => false)
         if (fUser) 
         {
-          User.findOneAndUpdate({id}, newUser).then(() => {
-          bot.sendMessage(msg.chat.id, `Successfully updated ${nick} 😍`)
-          })
+          bot.sendMessage(msg.chat.id, `You alraedy have a nick-name`)
         }
         else 
         {
@@ -37,6 +31,7 @@ module.exports = async (bot, msg) => {
                 in this way... "Nick ur_nickname"
                 • This is an irreversible action so please choose well. `)
             } 
+            
     
         default:
         break
